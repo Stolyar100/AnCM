b2f0232ab91d631ba5bfc0178afffb2654a92fb5
Stolyar100 <olejer555@gmail.com>
2020-10-06 21:21:16 +0300

    first try to solve problem

--- lab4/app.js
+++ lab4/app.js
@@ -1,69 +1,30 @@
-// const math = require('mathjs')
-// const Chart = require('chart.js')
-const math = window.math
-const Chart = window.Chart
+const math = require('mathjs')
 
-xList = [34.47, 34.53, 29.49, 34.20, 30.33, 33.85, 36.93, 34.68, 30.15, 30.12, 31.66, 36.88, 30.93, 28.72, 32.17, 35.44, 34.49, 34.19, 29.91, 36.42, 28.24, 33.42, 32.81]
+function taylorSeriesSin(x, accuracy) {
+  result = 0
+  i = 1
+  sinXSample = math.sin(x)
+  do {
+    // console.log("taylorSeriesSin -> difference", difference)
+    difference = math.abs(sinX - result)
+    result += (((- 1) ** (i - 1)) * (x ** (2 * i - 1))) / math.factorial( 2 * i - 1)
+    i += 1
+  } while (difference >= accuracy)
+  console.log("taylorSeriesSin -> i", i)
+  return result
+}
 
-pList = xList.map((currentValue, index, array) => array.filter(elemet => elemet === currentValue).length / array.length)
+x = 0.54
+sinExpression = (2 - x) ** 2
+accuracy1 = 0.0005
+accuracy2 = 0.001
 
-xMean = xList.reduce((accumulator, currentValue) => accumulator + currentValue, 0) / xList.length
-console.log('arithmetic mean: ', xMean)
+sinX = math.sin(sinExpression)
+result1 = taylorSeriesSin(sinExpression, accuracy1)
+result2 = taylorSeriesSin(sinExpression, accuracy2)
 
-xMeanSquare = xList.reduce((accumulator, currentValue) => accumulator + currentValue ** 2, 0) / xList.length
-console.log('root mean square: ', xMeanSquare)
-
-variance = xList.reduce((accumulator, currentValue) => accumulator + (currentValue - xMeanSquare) ** 2, 0) / xList.length
-console.log('Variance ', variance)
-
-skv = variance ** (1 / 2)
-console.log('SKV ', variance)
-
-alphabet = new Array()
-xList.forEach(element => {
-    if (!alphabet.includes(element)) alphabet.push(element)
-})
-entropy = -alphabet.reduce((accumulator, currentValue) => {
-    probability = xList.filter(elemet => elemet === currentValue).length / xList.length
-    return accumulator + probability * math.log(probability)
-}, 0)
-console.log('entropy: ', entropy)
-
-// creating chart on html page
-ctx = document.getElementById('chart').getContext('2d')
-
-var myChart = new Chart(ctx, {
-  type: 'bar',
-  data: {
-    labels: xList.map((element, index) => `x${index}`),
-    datasets: [
-      {
-        label: 'value of a discrete random variable',
-        data: xList,
-        backgroundColor: 'rgba(255, 99, 132, 0.2)',
-        borderColor: 'rgba(255, 99, 132, 1)',
-        borderWidth: 1
-      },
-      {
-        label: 'probability of a discrete random variable',
-        data: pList.map(element => element * 100),
-        backgroundColor: 'rgba(153, 102, 255, 0.2)',
-        borderColor: 'rgba(153, 102, 255, 1)',
-        borderWidth: 1
-      }
-    ]
-  },
-  options: {
-    responsive: true,
-    maintainAspectRatio: false,
-    scales: {
-      yAxes: [{
-        ticks: {
-          beginAtZero: true
-        }
-      }]
-    }
-  }
-})
-
-
+console.log("sinX: ", sinX)
+console.log(`result with accuracy ${accuracy1}: `, result1)
+console.log(`result with accuracy ${accuracy2}: `, result2)
+console.log('sinx - result1 = ', sinX - result1)
+console.log('sinx - result2 = ', sinX - result2)
