036c2c1016b807c89ccd7e975ca5964abcd8b66b
Stolyar100 <olejer555@gmail.com>
2020-10-11 20:43:13 +0300

    finished lab 5 script

--- lab5/app.js
+++ lab5/app.js
@@ -1,1 +1,45 @@
 const math = require('mathjs')
+
+let a = math.pi / 3
+let b = 3 * math.pi
+let n = 14
+let accuracy = 0.0005
+
+
+let area = getArea(a, b, n)
+console.log("area", area)
+let minimalInterval = getMinimalInerval(a, b, accuracy)
+console.log("minimalInterval", minimalInterval)
+
+
+function getMinimalInerval(a, b, accuracy) {
+  let interval = 2
+  let currentAccuracy = 0 
+  do {
+    I = getArea(a, b, interval)
+    I_2 = getArea(a, b, interval * 2)
+    currentAccuracy = math.abs(I_2 - I) / 15
+    interval++
+  } while (currentAccuracy > accuracy)
+  return interval
+} 
+
+function getArea (a, b, n) {
+  let I = 0
+  let V = 0
+  let h = (b - a) / n
+
+  for (x = a + h; x <= b - h; x += 2 * h) {
+    I += f(x)
+  }
+
+  for (x = a + 2 * h; x <= b - h; x += 2 * h) {
+    V += f(x)
+  }
+
+  return h * (f(a) + f(b) + 4 * I + 2 * V) / 3
+}
+
+function f(x) {
+  return x * math.pi * math.cos(8 * x) + x ** 3
+}
